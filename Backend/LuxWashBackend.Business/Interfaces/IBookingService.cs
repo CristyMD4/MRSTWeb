578bbe2fb61224d1da86dfad68fa3988f72b235d
MiraHirbu <mirahirbu@gmail.com>
2026-05-13 23:16:44 +0300

    Interface Auth and Booking

---
+++ Backend/LuxWashBackend.Business/Interfaces/IBookingService.cs
@@ -0,0 +1,11 @@
+﻿using LuxWashBackend.Domain.DTOs;
+
+namespace LuxWashBackend.Business.Interfaces
+{
+    public interface IBookingService
+    {
+        Task<BookingResponseDto> CreateAsync(int userId, CreateBookingDto dto);
+        Task<PagedResultDto<BookingResponseDto>> GetAllAsync(BookingQueryDto query);
+        Task<PagedResultDto<BookingResponseDto>> GetByUserIdAsync(int userId, BookingQueryDto query);
+    }
+}
