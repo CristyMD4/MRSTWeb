4b52df59155c59249c7718a82b3216b328789bf1
MiraHirbu <mirahirbu@gmail.com>
2026-05-13 23:56:12 +0300

    Creating bookings dto

---
+++ Backend/LuxWashBackend.Domain/DTOs/Bookings/CreateBookingDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuxWashBackend.Domain.DTOs
+{
+    public class CreateBookingDto
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int ServiceId { get; set; }
+
+        [Required]
+        public DateTime BookingDate { get; set; }
+    }
+}
