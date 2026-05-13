c3181121ace57e9f59a0fc53eac184e96fb466a3
MiraHirbu <mirahirbu@gmail.com>
2026-05-13 23:30:57 +0300

    Service interface

---
+++ Backend/LuxWashBackend.Business/Interfaces/IServiceService.cs
@@ -0,0 +1,13 @@
+﻿using LuxWashBackend.Domain.DTOs;
+
+namespace LuxWashBackend.Business.Interfaces
+{
+    public interface IServiceService
+    {
+        Task<PagedResultDto<ServiceResponseDto>> GetAllServicesAsync(ServiceQueryDto query);
+        Task<ServiceResponseDto?> GetServiceByIdAsync(int id);
+        Task<ServiceResponseDto> CreateServiceAsync(CreateServiceDto dto);
+        Task<ServiceResponseDto?> UpdateServiceAsync(int id, UpdateServiceDto dto);
+        Task<bool> DeleteServiceAsync(int id);
+    }
+}
