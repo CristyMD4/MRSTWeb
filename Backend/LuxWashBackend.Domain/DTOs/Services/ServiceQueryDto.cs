19df3dbe2c956c6754940fc8ab4bf3e86b63b638
MiraHirbu <mirahirbu@gmail.com>
2026-05-14 00:09:19 +0300

    Service query dto

---
+++ Backend/LuxWashBackend.Domain/DTOs/Services/ServiceQueryDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuxWashBackend.Domain.DTOs
+{
+    public class ServiceQueryDto
+    {
+        public string? Search { get; set; }
+        public bool? IsActive { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Page { get; set; } = 1;
+
+        [Range(1, 100)]
+        public int PageSize { get; set; } = 10;
+
+        [Range(0, double.MaxValue)]
+        public decimal? MinPrice { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal? MaxPrice { get; set; }
+    }
+}
