8036b2fc8851598df3c03551963c3d83cb6fedb1
MiraHirbu <mirahirbu@gmail.com>
2026-05-13 23:35:42 +0300

    Booking dto

---
+++ Backend/LuxWashBackend.Domain/DTOs/Bookings/BookingQueryDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuxWashBackend.Domain.DTOs
+{
+    public class BookingQueryDto
+    {
+        public string? Status { get; set; }
+        public int? ServiceId { get; set; }
+        public int? UserId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Page { get; set; } = 1;
+
+        [Range(1, 100)]
+        public int PageSize { get; set; } = 10;
+
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+    }
+}
