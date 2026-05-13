4ef39ffe71f78443328810d2b99ceb31e42082bf
MiraHirbu <mirahirbu@gmail.com>
2026-05-13 23:26:08 +0300

    Contact interface

---
+++ Backend/LuxWashBackend.Business/Interfaces/IContactService.cs
@@ -0,0 +1,10 @@
+﻿using LuxWashBackend.Domain.DTOs;
+
+namespace LuxWashBackend.Business.Interfaces
+{
+    public interface IContactService
+    {
+        Task<ContactMessageResponseDto> CreateAsync(CreateContactMessageDto dto);
+        Task<List<ContactMessageResponseDto>> GetAllAsync();
+    }
+}
