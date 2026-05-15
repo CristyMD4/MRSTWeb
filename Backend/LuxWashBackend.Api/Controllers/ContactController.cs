9e9704d168a303c560f1fdb10249e05afbe9194c
CristyMD4 <cristi.cornita@gmail.com>
2026-05-15 19:41:39 +0300

    Update ContactController.cs

---
+++ Backend/LuxWashBackend.Api/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using LuxWashBackend.Business.Interfaces;
+using LuxWashBackend.Domain.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuxWashBackend.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateContactMessageDto dto)
+        {
+            var message = await _contactService.CreateAsync(dto);
+            return Ok(message);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var messages = await _contactService.GetAllAsync();
+            return Ok(messages);
+        }
+    }
+}
