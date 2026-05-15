d6700ba78c25067c4a0278b50c578f7486ea76f1
CristyMD4 <cristi.cornita@gmail.com>
2026-05-15 19:41:44 +0300

    Update UserProfileDto.cs

---
+++ Backend/LuxWashBackend.Domain/DTOs/Auth/UserProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LuxWashBackend.Domain.DTOs
+{
+    public class UserProfileDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
