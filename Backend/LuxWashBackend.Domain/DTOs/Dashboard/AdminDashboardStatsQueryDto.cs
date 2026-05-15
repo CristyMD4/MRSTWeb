e4b9468333c46813ffc179d8a2d38c19dc70576f
CristyMD4 <cristi.cornita@gmail.com>
2026-05-15 19:41:44 +0300

    Update AdminDashboardStatsQueryDto.cs

---
+++ Backend/LuxWashBackend.Domain/DTOs/Dashboard/AdminDashboardStatsQueryDto.cs
@@ -0,0 +1,8 @@
+namespace LuxWashBackend.Domain.DTOs
+{
+    public class AdminDashboardStatsQueryDto
+    {
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+    }
+}
